70ce5878ac6e553b6a992a8e47b7123d659cf533
ankjai <ankit.jaiswal@appdirect.com>
2015-08-17 15:39:39 -0700

    Adding example of extend showing copy only once.

--- js/prototype_chains/jQuery_extend_func/extend_sample.js
+++ js/prototype_chains/jQuery_extend_func/extend_sample.js
@@ -20,3 +20,13 @@ objB = $.extend(true, objB, objA);
 
 console.log("a:" + objB.a);
 console.log("b:" + objB.b);
+
+
+// now update objA after using extend()
+objA.a = 10;
+// even though objA.a got updated
+console.log("objA.a:" + objA.a);
+// objB.a will still show old value
+// as the extend() copied all prop only ONCE
+console.log("a:" + objB.a);
+console.log("b:" + objB.b);
