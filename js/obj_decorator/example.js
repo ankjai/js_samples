4b06571b50cbb87a9a088bf0af2a967e88656c61
ankjai <ankit.jaiswal@appdirect.com>
2015-08-18 10:35:17 -0700

    Adding explanation of diff between class/decorator.

--- js/obj_decorator/example.js
+++ js/obj_decorator/example.js
@@ -1,4 +1,10 @@
-// decorator pattern
+/**
+ * carlike is a decorator as it accepts the
+ * object its going to augment.
+ * @param  {empty object} obj [description]
+ * @param  {int} loc [description]
+ * @return {augmented obj}     [description]
+ */
 var carlike = function(obj, loc) {
     // body...
     obj.loc = loc;
@@ -19,3 +25,10 @@ ben.move();
 // log
 console.log("amy loc:" + amy.loc);
 console.log("ben loc:" + ben.loc);
+
+
+/**
+ * The difference between the decorator and a class is
+ * decorator accepts the object it is going to augment whereas
+ * class builds the object that it is later going to augment.
+ */
